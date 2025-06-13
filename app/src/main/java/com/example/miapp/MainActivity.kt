282119f8a28c6dda493aad75035887eb6fc7770a
bottraker <jjimportado60@gmail.com>
2025-06-13 08:53:30 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/miapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.miapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this).apply {
+            text = "¡Mi APK generada con IA y GitHub!"
+            textSize = 24f
+            textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        }
+        setContentView(textView)
+    }
+}
